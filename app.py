d740933dbed800bb54a41d1cf52b526ab8d69e66
sdkinfotech <antonsedykh555@gmail.com>
2024-02-20 19:47:14 +0300

    REFACTORING Рефактринг функций, добавление комментариев в код в модуле app.py, moex_prices.py, в файлах .env и requirements.txt

--- app.py
+++ app.py
@@ -4,12 +4,19 @@ import os
 from time import sleep
 import psycopg2
 
-# Функция для ожидания доступности базы данных
+
 def wait_for_db(host, port, user, password, dbname):
-    conn = None
-    while conn is None:
+    
+    # Функция для ожидания доступности базы данных
+    # До начала работы приложения требуется проверить
+    # Доступно ли подключение к БД, если нет - вернуть ошибку
+
+    conn = None # устанавливаем флажок соединения на None
+    while conn is None: 
         try:
+            # попытка подключиться к БД
             print(f'Connecting to the database at host {host}...')
+            # записываем результат в conn
             conn = psycopg2.connect(
                 dbname=dbname,
                 user=user,
@@ -18,48 +25,54 @@ def wait_for_db(host, port, user, password, dbname):
                 port=port
             )
             print('Database connection established.')
-            return conn
-        except psycopg2.OperationalError as e:
-            print('Database connection failed:', e)
-            print('Waiting for the database...')
-            sleep(5)
+            return conn # Возвращаем значение conn
+        
+        except psycopg2.OperationalError as e: # обработка исключений
+            print('Database connection failed:', e) # Если соединение не удалось, вернуть в консоль исключение
+            print('Waiting for the database...') # сообщение перед следующей попыткой подключиться
+            sleep(5) 
 
 def main():
-    load_dotenv()
+    load_dotenv() # загружаем переменные среды
     
+    # подключаемся в БД в основной части программы
     dbname = os.getenv("DB_NAME")
     user = os.getenv("DB_USER")
     password = os.getenv("DB_PASSWORD")
-    host = os.getenv("DB_HOST")  # Используйте DB_HOST, если нет специального ключа для docker
+    host = os.getenv("DB_HOST") # этот хост подставляется если TEST_MODE=0
     port = os.getenv("DB_PORT")
 
     # проверяем, не включен ли TEST_MODE режим. Он используется для тестов на пайплайне
-    TEST_MODE = int(os.getenv("TEST_APP_MODE"))
-    print(f"TEST_MODE_STATUS: {TEST_MODE}")
-    if  TEST_MODE == 1:
-        host = "localhost" # переключаемся на localhost для тестов
+    TEST_MODE = int(os.getenv("TEST_APP_MODE")) # считываем значение
+    print(f"TEST_MODE_STATUS: {TEST_MODE}") # выводим статус 
+    if  TEST_MODE == 1: # если тест флаг активен, то
+        host = "localhost" # переключаемся на БД по адресу localhost для тестов.
 
+    # Возвращаем ответ соединения к БД в переменную conn
     conn = wait_for_db(host, port, user, password, dbname)
-    if conn is None:
+    if conn is None: # если None 
         print("Failed to establish database connection after multiple retries.")
-        exit(1)
+        exit(1) # завершаемся с кодом 1
 
-    # Создаем экземпляр класса fetcher
+    # Создаем экземпляр класса fetcher для получения тикеров и цен TQBR
+    # Таже передаем ему параметры подключения к БД для записи. 
+    # Также передаем название лог файла для логирования действий
     fetcher = MoexPriceTQBR(
         dbname=dbname,
         user=user,
         password=password,
         host=host,
         port=port,
-        logfile='log.txt'  # Указываем имя файла лога
+        logfile='log.txt'
     )
     
     # Интервал обновления — периодичность запуска в секундах.
+    # Устанавливаем необходимое время опроса. Это значение устанавливает 
+    # В приложении sleep до следующей итерации
     update_interval = int(os.getenv("TIME_SLEEP"))
     
-    # Бесконечный цикл для периодического выполнения задач
-    
-    while TEST_MODE == 0:
+    # Бесконечный цикл для периодического выполнения задач    
+    while TEST_MODE == 0: # если мы не в тестовом режиме
         print('Fetching new prices and updating the database...')
         fetcher.get_prices()
         fetcher.update_db()
@@ -68,7 +81,10 @@ def main():
         print(f"Sleeping for {update_interval} seconds.")
         sleep(update_interval)  # Ожидаем заданный интервал перед следующим обновлением.
     else:
-        # Тестовый режим - исполняем код один раз
+        # Иначе если 1 или все что угодно, то тестовый режим - исполняем код один раз
+        # Эта логика реализована для того чтобы приложение не зацикливалось на выполнении тестирования
+        # Нам требуется один раз пройти итерацию, чтобы вернуть успешное выполнение и перейти к следующим 
+        # шагам пайплайна.
         print("WARNING! TEST MODE ON")
         print('Running in test mode (one-time execution)...')
         fetcher.get_prices()
@@ -76,5 +92,6 @@ def main():
         fetcher.show_prices()
         fetcher.calculate_null_prices_percentage()
 
+# выполняем main
 if __name__ == "__main__":
     main()
--- parsers/moex_prices.py
+++ parsers/moex_prices.py
@@ -6,6 +6,35 @@ from datetime import datetime
 import os
 
 class MoexPriceTQBR:
+    """
+    __init__: 
+    инициализирует экземпляр класса с переданными 
+    параметрами для доступа к базе данных и файлу лога.
+
+    log_message: 
+    логирует сообщения, принимая строку сообщения 
+    и записывая его в файл лога с временной меткой.
+
+    get_prices: 
+    запрашивает данные о котировках 
+    с Московской биржи и сохраняет их как JSON.
+
+    update_db: 
+    обновляет базу данных, вставляя 
+    полученные данные о ценах.
+
+    show_prices: 
+    читает данные из файла JSON и логирует 
+    информацию о текущих ценах акций.
+
+    save_null_prices_percentage_to_db: 
+    сохраняет рассчитанный процент нулевых цен в базу данных.
+
+    calculate_null_prices_percentage: 
+    рассчитывает процент нулевых цен на основе 
+    полученных данных и вызывает метод сохранения в базе данных.
+    """
+
     def __init__(self, dbname, user, password, host, port, logfile='log.txt'):
         self.dbname = dbname
         self.user = user
@@ -78,20 +107,17 @@ class MoexPriceTQBR:
 
     def show_prices(self):
         try:
-            # Loading data from the JSON file
             with open('latest_prices.json', 'r', encoding='utf-8') as json_file:
                 self.latest_data = json.load(json_file)
             
-            # Checking for the presence of data
             if self.latest_data:
-                # Getting the time of the last record from the JSON file
+
                 last_updated = datetime.fromtimestamp(os.path.getmtime('latest_prices.json'))
                 self.log_message(
                     f"SHOW LATEST: Current information as of {last_updated.strftime('%Y-%m-%d %H:%M:%S')}")
                 
                 for security in self.latest_data:
                     ticker, price = security[0], security[1]
-                    # print(f"Ticker: {ticker}, Price: {price}")
                     self.log_message(f"Ticker: {ticker}, Price: {price}")
             else:
                 self.log_message("No data to display. The get_prices method was not run.")
@@ -99,7 +125,7 @@ class MoexPriceTQBR:
             self.log_message(f"Error reading data from the JSON file: {e}")
 
     def save_null_prices_percentage_to_db(self, null_price_percentage):
-        # Подключаемся к базе данных
+
         conn = None
         try:
             conn = psycopg2.connect(
@@ -111,20 +137,16 @@ class MoexPriceTQBR:
             )
             cursor = conn.cursor()
 
-            # SQL запрос для вставки данных в таблицу
             insert_query = """
             INSERT INTO null_price_percent_metrics (calculation_date, calculation_time, percent_null_prices) 
             VALUES (%s, %s, %s);
             """
-            # Текущая дата и время для регистрации в базе данных
             now = datetime.now()
             current_date = now.strftime("%Y-%m-%d")
             current_time = now.strftime("%H:%M:%S")
 
-            # Выполнить запрос на вставку данных
             cursor.execute(insert_query, (current_date, current_time, null_price_percentage))
 
-            # Подтвердить изменения
             conn.commit()
             self.log_message(f"Null price percentage data successfully added to the database - info")
 
@@ -132,25 +154,19 @@ class MoexPriceTQBR:
             self.log_message(f"Error saving null price percentage to database: {e} - error")
 
         finally:
-            # Закрываем соединение с базой данных
-            if conn:  # проверяем, что `conn` не равно `None`
+
+            if conn:  
                 cursor.close()
                 conn.close()
 
-    # Теперь нужно вызвать этот метод в конце метода `calculate_null_prices_percentage`:
     def calculate_null_prices_percentage(self):
+        
         if not self.latest_data:
             self.log_message("No data to analyze. The get_prices method was not run. - error")
             return
 
         total_count = len(self.latest_data)
         null_price_count = sum(1 for item in self.latest_data if item[1] is None)
-
-        # Вычисление процента значений null
         null_price_percentage = (null_price_count / total_count) * 100 if total_count else 0
-
-        # Запись результата в лог
         self.log_message(f"Percent of tickers with null price: {null_price_percentage:.2f}% - info")
-
-        # Сохраняем результат в базе данных
         self.save_null_prices_percentage_to_db(null_price_percentage)
