b732ecd9632f766d9c43e04d05600510bc1ba11f
sdkinfotech <antonsedykh555@gmail.com>
2024-02-20 09:26:34 +0300

    изменена проверка доступности базы данных, ограничил количество попыток подключения в app.py, изменены настройки инициализации postgres как сервис в ci_cd.yml

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 import psycopg2
 from time import sleep
 
+
 def wait_for_db(host, port, user, password, dbname):
     conn = None
     while conn is None:
@@ -17,58 +18,56 @@ def wait_for_db(host, port, user, password, dbname):
                 port=port
             )
             print('Database connection established.')
+            return conn  # Возвращаем объект подключения для дальнейшего использования.
         except psycopg2.OperationalError as e:
             print('Database connection failed:', e)
             print('Waiting for the database...')
             sleep(5)
-        finally:
-            if conn is not None:
-                conn.close()
 
+# В основной функции теперь есть логика для прерывания попыток после TEST_MODE.
 def main():
-    
     load_dotenv()
     
-    # Получение значений переменных окружения
+    # Базовая конфигурация подключения к БД.
     dbname = os.getenv("DB_NAME")
     user = os.getenv("DB_USER")
     password = os.getenv("DB_PASSWORD")
     host = os.getenv("DB_DOCKER_HOST")
     port = os.getenv("DB_DOCKER_PORT")
-    
-    # Check if TEST_MODE is set to 'true' in the environment variables
+
+    # Если установлена переменная TEST_MODE, используем localhost.
     if os.getenv("TEST_MODE") == "true":
         host = "localhost"
+    
+    # Ожидание готовности базы данных и подключение к ней.
+    conn = wait_for_db(host, port, user, password, dbname)
+    if conn is None:
+        print("Failed to establish database connection after multiple retries.")
+        exit(1)  # Завершение приложения с ошибкой, если не удалось подключиться.
 
-    # Бесконечный цикл работы приложения
+    # Выполнение операций с MOEX и базой данных.
+    fetcher = MoexPriceTQBR(
+        dbname=dbname,
+        user=user,
+        password=password,
+        host=host,
+        port=port
+    )
+
+    print('Fetching new prices and updating the database...')
+    fetcher.get_prices()
+    fetcher.update_db()
+    fetcher.show_prices()
+    fetcher.calculate_null_prices_percentage()
+
+    # Если TEST_MODE активен, завершаем после одной итерации.
+    if os.getenv("TEST_MODE") == "true":
+        return
+
+    # В противном случае продолжаем выполнение в бесконечном цикле.
     while True:
-        # Ожидание готовности базы данных
-        wait_for_db(host, port, user, password, dbname)
+        sleep(int(os.getenv("TIME_SLEEP", "300")))  # Пауза перед следующим обновлением.
 
-        # Создание экземпляра класса для соединения с MOEX и получения данных TQBR
-        fetcher = MoexPriceTQBR(
-            dbname=dbname,
-            user=user,
-            password=password,
-            host=host,
-            port=port
-        )
-
-        # Выполнение операций с MOEX и базой данных
-        print('Fetching new prices and updating the database...')
-        fetcher.get_prices()  # Запрос к MOEX и сохранение в формате JSON
-        fetcher.update_db()   # Обновление базы данных из JSON
-        fetcher.show_prices() # Отображение данных в консоли из JSON
-        fetcher.calculate_null_prices_percentage() # Расчет процента отсутствующих цен
-
-        # В режиме тестирования выполняем только один раз и затем выходим
-        if os.getenv("TEST_MODE") == "true":
-            break
-
-        # Пауза перед следующим запуском цикла
-        delay = int(os.getenv("TIME_SLEEP", "300"))
-        print(f'Waiting for {delay} seconds before the next update...')
-        sleep(delay)
 
 if __name__ == "__main__":
     main()
