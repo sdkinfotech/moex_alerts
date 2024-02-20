bddb989fd4c0dd06270673648bf1ab8507c28643
sdkinfotech <antonsedykh555@gmail.com>
2024-02-20 17:25:58 +0300

    FIX добавлен  update_interval в app.py и вызовы функций fetch.get_price() update_db() и тд в цикл while

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from parsers.moex_prices import MoexPriceTQBR
 from dotenv import load_dotenv
 import os
-import psycopg2
 from time import sleep
 
-
+# Функция для ожидания доступности базы данных
 def wait_for_db(host, port, user, password, dbname):
     conn = None
     while conn is None:
@@ -18,56 +17,52 @@ def wait_for_db(host, port, user, password, dbname):
                 port=port
             )
             print('Database connection established.')
-            return conn  # Возвращаем объект подключения для дальнейшего использования.
+            return conn
         except psycopg2.OperationalError as e:
             print('Database connection failed:', e)
             print('Waiting for the database...')
             sleep(5)
 
-# В основной функции теперь есть логика для прерывания попыток после TEST_MODE.
 def main():
     load_dotenv()
     
-    # Базовая конфигурация подключения к БД.
     dbname = os.getenv("DB_NAME")
     user = os.getenv("DB_USER")
     password = os.getenv("DB_PASSWORD")
-    host = os.getenv("DB_DOCKER_HOST")
-    port = os.getenv("DB_DOCKER_PORT")
+    host = os.getenv("DB_HOST")  # Используйте DB_HOST, если нет специального ключа для docker
+    port = os.getenv("DB_PORT")
 
-    # Если установлена переменная TEST_MODE, используем localhost.
     if os.getenv("TEST_MODE") == "true":
         host = "localhost"
-    
-    # Ожидание готовности базы данных и подключение к ней.
+
     conn = wait_for_db(host, port, user, password, dbname)
     if conn is None:
         print("Failed to establish database connection after multiple retries.")
-        exit(1)  # Завершение приложения с ошибкой, если не удалось подключиться.
+        exit(1)
 
-    # Выполнение операций с MOEX и базой данных.
+    # Создаем экземпляр класса fetcher
     fetcher = MoexPriceTQBR(
         dbname=dbname,
         user=user,
         password=password,
         host=host,
-        port=port
+        port=port,
+        logfile='moex_price_log.txt'  # Указываем имя файла лога
     )
-
-    print('Fetching new prices and updating the database...')
-    fetcher.get_prices()
-    fetcher.update_db()
-    fetcher.show_prices()
-    fetcher.calculate_null_prices_percentage()
-
-    # Если TEST_MODE активен, завершаем после одной итерации.
-    if os.getenv("TEST_MODE") == "true":
-        return
-
-    # В противном случае продолжаем выполнение в бесконечном цикле.
+    
+    # Интервал обновления — периодичность запуска в секундах.
+    update_interval = int(os.getenv("TIME_SLEEP"))
+    
+    # Бесконечный цикл для периодического выполнения задач
     while True:
-        sleep(100)  # Пауза перед следующим обновлением.
+        print('Fetching new prices and updating the database...')
+        fetcher.get_prices()
+        fetcher.update_db()
+        fetcher.show_prices()
+        fetcher.calculate_null_prices_percentage()
 
+        print(f"Sleeping for {update_interval} seconds.")
+        sleep(update_interval)  # Ожидаем заданный интервал перед следующим обновлением.
 
 if __name__ == "__main__":
     main()
